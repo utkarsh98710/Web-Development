6b5686a01a33f7dcdc520c03e9c85ecebfc8d201
utkarsh98710 <156845520+utkarsh98710@users.noreply.github.com>
2024-03-02 17:38:38 +0530

    Add files via upload

---
+++ class3.cpp
@@ -0,0 +1,217 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // // declare a integer type variable
+    // int score;
+    // // print enter the score
+    // cout<<"Enter the score"<<endl;
+    // // Take input into score variable
+    // cin >> score;
+    // // if condition
+    // if(score<300)
+    // {
+    //     cout<<"India Wins"<< endl;
+    // }
+    // else{
+    //     cout<<"Pak Wins"<< endl;
+    // }
+
+// #second Question
+
+    // int age;
+    // cout<< "enter your age" << endl;
+    // cin>>age;
+    // if(age>18){
+    //     cout << "We eligble to vote" << endl;
+    // }
+    // else{
+    //     cout << "We are not eligble to vote" << endl;
+    // }
+
+
+//     int marks;
+//     cin>> marks;
+//     if(marks>=90){
+//         cout<< "A grade";
+//     }
+//     else{
+//         if(marks >=80){
+//             cout << "B grade";
+//         }
+//         else{
+//             if (marks>=60){
+//                 cout<< "C grade";
+//             }
+//             else{
+//                 if (marks>=40){
+//                 cout<< "D grade";
+//             }
+//             else{
+//                 cout<<"F grade";
+//             }
+//         }
+//     }
+// }
+
+// }
+
+
+// int bronum;
+// cin>>bronum;
+// if(bronum==0){
+//     cout<<"Baat banjayegi";
+// }
+// else{
+//     cout<<"Baat nahi banegi";
+// }
+
+// int n;
+// if(cin>>n)
+// {
+//     cout<<"Gupta";
+// }
+
+
+// if(cout<<"Gutpa")
+// {
+//     Cout<<"Utkarsh";
+// }
+
+// pattern of solid rectangle
+
+// for(int row=0; row<3; row=row+1)
+// {
+//     for(int col=0; col<5; col=col+1)
+//     {
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+// }
+
+//   solid square pattern
+// int n;
+// cin>>n;
+// for( int row=0; row<n; row =row+1)
+// {
+//     for( int col=0; col<n; col=col+1)
+//     {
+//         cout<<"* ";
+//     }
+//     cout<< endl;
+// }
+  
+//   Hollow rectangle
+// int rowcount, colcount;
+// cin >> rowcount;
+// cin >> colcount;
+
+//     for(int row=0; row<rowcount; row=row+1){
+//         if(row==0 || row==rowcount-1){
+//             for(int col=0; col<colcount; col=col+1){
+//                 cout << "* ";
+//             }
+//         }
+//         else{
+//             cout <<"* ";
+//             for(int i=0; i<colcount-2; i=i+1){  
+//                 cout<<"  ";
+//             }
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+    
+    // Hollow rectangle for 6 row
+    // for(int row=0; row<6; row=row+1){
+    //     if(row==0 || row==5){
+    //         for(int col=0; col<5; col=col+1){
+    //             cout<<"* ";
+    //         }
+    //     }
+    //     else{
+    //         cout<<"* ";
+    //         for( int col=0; col<3; col=col+1){
+    //             cout<<"  ";
+    //         }
+    //         cout<<"* ";
+    //     }
+    //     cout<< endl;
+    // }
+//    Half  pyramid  
+    // int n;
+    // cin >> n;
+    // for(int row=0; row<n; row=row+1){
+    //     for(int col=0; col<row+1; col=col+1){
+    //         cout<< "* ";
+    //     }
+    //     cout<< endl;
+    // }
+
+    // Inverted Half Pyramid
+
+    // int n;
+    // cin>> n;
+    // for(int row=0; row<n; row=row+1){
+    //     for(int col=0; col<n-row; col=col+1){
+    //         cout<< "* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // Numreical Half Pyramid
+    // int n;
+    // cin >> n;
+    // for(int row=0; row<n; row=row+1){
+    //     for(int col=0; col<row+1; col=col+1){
+    //         cout<< col+1;
+    //     }
+    //     cout<< endl;
+    // }
+
+    // Inverted Half Triangle
+    // int n;
+    // cin >>n;
+    // for(int row=0; row<n; row=row+1){
+    //     for(int col=0; col<n-row; col=col+1){
+    //         cout<< col+1;
+    //     }
+    //     cout<< endl;
+    // }
+
+//    hollow rectangle
+//    int n;
+//    cin>>n;
+//    for(int i=0; i<n; i++){
+//     for(int j=0; j<n; j++){
+//         if(i==0 || i==n-1 || j==0 ||j==n-1){
+//             cout<<"* ";
+//         }
+//         else{
+//             cout<<"  ";
+//         }
+//     }
+//     cout<<endl;
+//    }
+
+
+//   Hollow inverted pyramid
+//    int n;
+//    cin>>n;
+//    for(int i=0; i<n; i++){
+//     for( int j=0; j<n; j++){
+//         if(i==0 || j==0 || j==n-i-1){
+//             cout<<"* ";
+//         }
+//         else{
+//         cout<<"  ";
+//         }
+//     }
+//     cout<<endl;
+//    }
+
+}
+
+
+
+
+
